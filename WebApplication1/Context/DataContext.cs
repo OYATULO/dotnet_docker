938c0993333574eb568552c90575c8efd4297a1d
OYATULO <oyatullo.sangov1998@mail.ru>
2023-01-30 15:56:53 +0300

    data Context

--- WebApplication1/Context/DataContext.cs
+++ WebApplication1/Context/DataContext.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
              
-            string connection = @"Data Source=sql.bsite.net\MSSQL2016;Initial Catalog=oyatullo_db; User ID = oyatullo_db; Password = 1357986420; "; //Integrated Security = True
+            string connection = @"Data Source=sql.bsite.net\MSSQL2016;Initial Catalog=oyatullo_db; User ID = oyatullo_db; Password =; "; //Integrated Security = True
             optionsBuilder.UseSqlServer(connection);
         }
         public DbSet<Students> _Students { get; set; }
